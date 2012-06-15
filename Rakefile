3609a67683bae4227ed9cc809438fb2dfb0e58d0
Mark Stanislav <mark.stanislav@gmail.com>
2012-06-15 19:59:50 -0400

    Added CoreLocation, MapKit, and QuartzCore to the Rakefile; built pin-placement for configured location. Fixes #16 and #18.

--- Rakefile
+++ Rakefile
@@ -6,5 +6,5 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = "Let's Camp"
   app.interface_orientations = [:portrait]
-  app.frameworks += ['CoreData','CoreImage']
+  app.frameworks += ['CoreLocation', 'MapKit', 'CoreData', 'QuartzCore','CoreImage']
 end
--- app/map_controller.rb
+++ app/map_controller.rb
@@ -2,6 +2,14 @@ class MapController < UIViewController
 
   def viewDidLoad
     @@lat, @@lng = getCoordinates("1600%20Pennsylvania%20Avenue%20Washington%20DC")
+    coordinate = CLLocationCoordinate2D.new(@@lat,@@lng)
+
+    mapView = MKMapView.alloc.initWithFrame(view.bounds)
+    mapView.delegate = self
+    view.addSubview(mapView)
+
+    setRegion(mapView, coordinate)
+    setPin(mapView, coordinate)
   end
 
   def getCoordinates(address)
@@ -19,6 +27,23 @@ class MapController < UIViewController
     end
 
     return [json['results'].first[:geometry][:location][:lat],json['results'].first[:geometry][:location][:lng]]
+  end
+
+  def setRegion(map, coordinate)
+    span = MKCoordinateSpan.new(0.52, 0.52)
+    region = MKCoordinateRegion.new(coordinate, span)
+    map.setRegion(region, animated: "YES")
+  end
+
+  def setPin(map, coordinate)
+    marker = MKPointAnnotation.alloc.init
+    marker.setCoordinate(coordinate)
+
+    pin_view = MKPinAnnotationView.alloc.initWithAnnotation(@marker, reuseIdentifier: "id")
+    pin_view.animatesDrop = true
+
+    map.addAnnotation(marker)
+    marker.setTitle('Campground')
   end
 
 end
