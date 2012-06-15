f7d6641aa31cb4e0944e7aa80f08167e5fc525f9
Mark Stanislav <mark.stanislav@gmail.com>
2012-06-15 17:44:22 -0400

    Added basic JSON parsing support for a URL; result transforms an address to lat/lon coords and returns a multi-array back and is mapped to class vars. Fixes #14 and #15.

--- app/map_controller.rb
+++ app/map_controller.rb
@@ -1,6 +1,24 @@
 class MapController < UIViewController
 
   def viewDidLoad
+    @@lat, @@lng = getCoordinates("1600%20Pennsylvania%20Avenue%20Washington%20DC")
+  end
+
+  def getCoordinates(address)
+    url = "http://maps.google.com/maps/api/geocode/json?address=#{address}&sensor=false"
+
+    error_ptr = Pointer.new(:object)
+    data = NSData.alloc.initWithContentsOfURL(NSURL.URLWithString(url), options:NSDataReadingUncached, error:error_ptr)
+    unless data
+      raise error_ptr[0]
+    end
+
+    json = NSJSONSerialization.JSONObjectWithData(data, options:0, error:error_ptr)
+    unless json
+      raise error_ptr[0]
+    end
+
+    return [json['results'].first[:geometry][:location][:lat],json['results'].first[:geometry][:location][:lng]]
   end
 
 end
