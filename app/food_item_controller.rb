58399ab0e8fe229f3bbb0af1f2763a941e56e53e
Mark Stanislav <mark.stanislav@gmail.com>
2012-06-16 22:59:48 -0400

    Added textbox labels and text placeholders for food and supplies controllers. Fixes #37 and #42.

--- app/food_item_controller.rb
+++ app/food_item_controller.rb
@@ -7,8 +7,11 @@ class FoodItemController < UIViewController
   end
 
   def viewDidLoad
-    @name = create_text_field(placeholder: 'Enter an item name', frame: [[10, 20], [300, 30]])
-    @quantity = create_text_field(placeholder: 'Enter am item quantity', frame: [[10, 90], [300, 30]])
+    create_label(label: 'Enter an item name', frame: [[10, 10], [300, 30]])
+    @name = create_text_field(placeholder: 'e.g. Apples or Crackers', frame: [[10, 40], [300, 30]])
+
+    create_label(label: 'Enter a quantity', frame: [[10, 80], [300, 30]])
+    @quantity = create_text_field(placeholder: 'e.g. 10 or 1 Box', frame: [[10, 110], [300, 30]])
   end
 
   def viewWillAppear(animated)
@@ -40,9 +43,18 @@ private
     text_field.textAlignment = UITextAlignmentCenter
     text_field.textColor = UIColor.blackColor
     text_field.borderStyle = UITextBorderStyleRoundedRect
-    text_field.keyboardType = params[:keyboardType] if params[:keyboardType]
     text_field.frame = params[:frame]
     view.addSubview(text_field)
     text_field
+  end
+
+  def create_label(params)
+    label = UILabel.new
+    label.font = UIFont.systemFontOfSize(20)
+    label.text = params[:label]
+    label.textColor = UIColor.blackColor
+    label.backgroundColor = UIColor.clearColor
+    label.frame = params[:frame]
+    view.addSubview(label)
   end
 end
